ed09af2cefe57a8203b055b3c510f8e1e3edd3d4
Fagner Braga <fagner.fgb@gmail.com>
2023-09-24 09:14:31 -0300

    Delete W2019 directory

--- W2019/03-GROUPS/cria_grupos_ad.cmd
+++
@@ -1,3 +0,0 @@
-:: CRIA GRUPOS BASEADO EM ARQUIVO
-FOR /F "EOL=; TOKENS=1,2,* DELIMS=," %%I IN (GRU.TXT) DO dsadd group "CN=%%I,OU=GROUPS,OU=GRU,OU=FGB,DC=FGB,DC=LOCAL" -samid %%I -secgrp yes -scope g
-FOR /F "EOL=; TOKENS=1,2,* DELIMS=," %%I IN (OSC.TXT) DO dsadd group "CN=%%I,OU=GROUPS,OU=OSC,OU=FGB,DC=FGB,DC=LOCAL" -samid %%I -secgrp yes -scope g
